761072842e4ea0acac1e8c951cb58c984229fc5e
Andrew Bubnov <jackblack.nt@ya.ru>
2020-11-15 15:52:07 +0500

    Graphql http added

--- server/server/app.js
+++ server/server/app.js
@@ -1,7 +1,10 @@
-const expres = require('express')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
 
 const app = express()
 const PORT = 3005
+
+app.use('/graphql', graphqlHTTP({}))
 
 app.listen(PORT, err => {
   err ? console.log(error) : console.log('Server started!')
