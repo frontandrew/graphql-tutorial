9f30aef6c01c1d302af23c108d2c72f5eddfc7b4
Andrew Bubnov <jackblack.nt@ya.ru>
2020-11-15 18:09:13 +0500

    Collections movies and dirrectors have been connected

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,10 +8,10 @@ const {
 } = graphql
 
 const movies = [
-  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
-  { id: '2', name: '1984', genre: 'Sci-Fi' },
-  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller' },
-  { id: '4', name: 'Snatch', genre: 'Crime-Comedy' },
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4' },
 ]
 
 const directors = [
